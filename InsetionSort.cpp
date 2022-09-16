7c5cddaf3374bcfd3c74ad825f19281aa9a183e3
Pramodh Krishna <96466131+Pammu10@users.noreply.github.com>
2022-09-16 15:48:20 +0530

    Update InsetionSort.cpp

--- InsetionSort.cpp
+++ InsetionSort.cpp
@@ -8,7 +8,7 @@ void insertion_sort(int arr[], int n){
     for (int j = 1; j < n; j++){
         int key = arr[j];
         int i = j - 1;
-        while (i >= 0 && arr[i] < key){
+        while (i >= 0 && arr[i] > key){
             arr[i+1] = arr[i];
             i--;
         }
