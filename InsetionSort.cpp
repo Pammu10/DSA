533277c61a2447cca081e2c68cfb98a40d2c97f5
Pramodh Krishna <96466131+Pammu10@users.noreply.github.com>
2022-09-16 15:49:16 +0530

    Update InsetionSort.cpp

--- InsetionSort.cpp
+++ InsetionSort.cpp
@@ -4,15 +4,18 @@
 using namespace std;
 
 void insertion_sort(int arr[], int n){
-    
-    for (int j = 1; j < n; j++){
-        int key = arr[j];
-        int i = j - 1;
-        while (i >= 0 && arr[i] > key){
-            arr[i+1] = arr[i];
-            i--;
+    for (int i = 1; i < n; i++){
+        int temp = arr[i];
+        for (int j = 0; j < i; j++){
+            if (arr[i] < arr[j]){
+                int k;
+                for ( k= i; k > j; k--){
+                    arr[k] = arr[k - 1];
+                }
+                arr[k] = temp;  
+             }
+             
         }
-        arr[i + 1] = key;
     }
 }
 int main()
