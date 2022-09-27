4b1655445606381887b8fddf377ce9cef1dcf9f4
Pramodh Krishna <96466131+Pammu10@users.noreply.github.com>
2022-09-27 20:32:14 +0530

    Update and rename InsetionSort.cpp to InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,41 @@
+//Pramodh 
+//Insertion Sort having time complexity 0(n^2)
+
+#include<stdio.h>  
+
+void insertion_sort(int arr[], int n){
+   int i, j, k;
+   for (i = 1; i < n; i++){
+      int temp = arr[i];
+      for (j = 0; j < i; j++){
+         while (temp < arr[j]){
+            int k;
+            for (k = i; k > j; k--){
+               arr[k] = arr[k - 1];
+            }
+            arr[k] = temp;
+         }
+      }
+   }
+}
+
+int main(){
+   int n, item;
+   printf("Enter number of elements in your array: ");
+   scanf("%d", &n);
+   int arr[n];
+   for (int i = 0; i < n; i++){
+      scanf("%d", &arr[i]);
+   }
+   printf("Array before sorting\n");
+   for (int i = 0; i < n; i++){
+      printf("%d ", arr[i]);
+   }
+   insertion_sort(arr, n);
+   printf("\nArray after sorting\n");
+   for (int i = 0; i< n; i++){
+      printf("%d ", arr[i]);
+   }
+   return 0;
+   
+}
