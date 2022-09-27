184a2febb997f067d9154cbcdd25990fbffdc63d
Pramodh Krishna <96466131+Pammu10@users.noreply.github.com>
2022-09-27 20:21:49 +0530

    Update and rename BubbleSort.cpp to BubbleSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,38 @@
+//Pramodh Krishna
+//Bubble sort
+
+#include<stdio.h>  
+
+void bubble_sort(int arr[], int n){
+   int i, j;
+   for (i = 0; i < n; i++){
+      for (j = 0; j < n - 1 - i; j++){
+         if (arr[j] > arr[j + 1]){
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+         }
+      }
+   }
+}
+
+int main(){
+   int n, item;
+   printf("Enter number of elements in your array: ");
+   scanf("%d", &n);
+   int arr[n];
+   for (int i = 0; i < n; i++){
+      scanf("%d", &arr[i]);
+   }
+   printf("Array before sorting\n");
+   for (int i = 0; i < n; i++){
+      printf("%d ", arr[i]);
+   }
+   bubble_sort(arr, n);
+   printf("\nArray after sorting\n");
+   for (int i = 0; i< n; i++){
+      printf("%d ", arr[i]);
+   }
+   return 0;
+   
+}
