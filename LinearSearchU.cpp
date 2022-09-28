4a0afb96d556ac6bb5f05bce2fbff30938c18e51
Pramodh Krishna <96466131+Pammu10@users.noreply.github.com>
2022-09-28 07:12:22 +0530

    Update LinearSearchU.cpp

--- LinearSearchU.cpp
+++ LinearSearchU.cpp
@@ -1,29 +1,31 @@
 //Pramodh Krishna
-//Linear Search for unsorted array
-#include<iostream>
-using namespace std;
+//Linear search for unsorted array having time complexity O(n)
 
-void linear_search(int a[], int n, int k)  
-{    
-    bool present = false;
-    for (int i = 0; (i < n) && (present == false); i++){
-        if (a[i] == k){
-            present = true;
-            cout << k << " found at position " << i+1 << endl;
-        }  
+#include<stdio.h>
+
+int linear_search(int arr[], int n, int item){
+    int found = 0;
+    for (int i = 0; i < n; i++){
+        if (arr[i] == item){
+            found = 1;
+            printf("Item found at position %d", i + 1);
+            break;
+        }
     }
-    if (present == false)
-        cout << "Not found" << endl;
-    
-}   
+    if (found == 0)
+        printf("Item not found in the array");
+}
 
-int main()
-{
-    int arr[] = {31, 41, 59, 26, 41, 58};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int k;
-    printf("Enter the number you want to find: ");
-    scanf("%d", &k);
-    linear_search(arr, n, k);
+int main(){
+    int n, item;
+    printf("Enter number of elements in your array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the item you want to search for in the array: ");
+    scanf("%d", &item);
+    linear_search(arr, n, item);
     return 0;
 }
