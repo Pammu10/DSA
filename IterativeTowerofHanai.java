f98f3619f8413569f70f119fb6d41107483c3e0a
Pramodh <pramodh380@gmail.com>
2024-02-14 20:29:47 +0530

    Done with cat1

--- IterativeTowerofHanai.java
+++ IterativeTowerofHanai.java
@@ -1,162 +1,83 @@
-/**
- * IterativeTowerofHanai
- */
-// Java program for iterative 
-// Tower of Hanoi
-class IterativeTowerofHanai{
+import java.util.*;
+class Main{    
+	class Stack{    	
+		int capacity;
+		int top;
+		int array[];    
+	}    
+	Stack createStack(int capacity){    	
+		Stack stack = new Stack();
+		stack.capacity = capacity;
+		stack.top = -1;
+		stack.array = new int[capacity];
+		return stack;
+	}
 	
-    // A structure to represent a stack
-    class Stack
-    {
-        int capacity;
-        int top;
-        int array[];
-    }
-    
-    // Function to create a stack of given capacity.
-    Stack createStack(int capacity)
-    {
-        Stack stack = new Stack();
-        stack.capacity = capacity;
-        stack.top = -1;
-        stack.array = new int[capacity];
-        return stack;
-    }
-    
-    // Stack is full when the top is equal
-    // to the last index
-    boolean isFull(Stack stack)
-    {
-        return (stack.top == stack.capacity - 1);
-    }
-    
-    // Stack is empty when top is equal to -1
-    boolean isEmpty(Stack stack)
-    {
-        return (stack.top == -1);
-    }
-    
-    // Function to add an item to stack.It 
-    // increases top by 1
-    void push(Stack stack, int item)
-    {
+	static boolean isFull(Stack stack){    	
+		return (stack.top == stack.capacity - 1);
+	}
+	
+	static boolean isEmpty(Stack stack){
+		return (stack.top == -1);
+	}
+    static void push(Stack stack, int item){    	
         if (isFull(stack))
             return;
-            
-        stack.array[++stack.top] = item;
+        stack.top++;
+        stack.array[stack.top] = item;    
     }
     
-    // Function to remove an item from stack.It
-    // decreases top by 1
-    int pop(Stack stack)
-    {
+    static int pop(Stack stack){
         if (isEmpty(stack))
             return Integer.MIN_VALUE;
-            
         return stack.array[stack.top--];
     }
     
-    // Function to implement legal movement between
-    // two poles
-    void moveDisksBetweenTwoPoles(Stack src, Stack dest, 
-                                char s, char d) 
-    {
-        int pole1TopDisk = pop(src);
-        int pole2TopDisk = pop(dest);
-    
-        // When pole 1 is empty
-        if (pole1TopDisk == Integer.MIN_VALUE) 
-        {
-            push(src, pole2TopDisk);
-            moveDisk(d, s, pole2TopDisk);
-        }
-        
-        // When pole2 pole is empty
-        else if (pole2TopDisk == Integer.MIN_VALUE) 
-        {
-            push(dest, pole1TopDisk);
-            moveDisk(s, d, pole1TopDisk);
-        }
-        
-        // When top disk of pole1 > top disk of pole2
-        else if (pole1TopDisk > pole2TopDisk) 
-        {
-            push(src, pole1TopDisk);
-            push(src, pole2TopDisk);
-            moveDisk(d, s, pole2TopDisk);
-        }
-        // When top disk of pole1 < top disk of pole2
-        else
-        {
-            push(dest, pole2TopDisk);
-            push(dest, pole1TopDisk);
-            moveDisk(s, d, pole1TopDisk);
-        }
+    static void move_disc(Stack source, Stack destination, char s, char d){
+    int p1= pop(source);
+    int p2 = pop(destination);
+    if (p1== Integer.MIN_VALUE){
+        push(source, p2);
+        System.out.println("Move the disk "+p2+ " from "+d+ " to "+s);
+    }    
+    else if (p2 == Integer.MIN_VALUE){    		
+        push(destination, p1);
+        System.out.println("Move the disk "+p1+ " from "+s+" to "+d);
     }
-    
-    // Function to show the movement of disks
-    void moveDisk(char fromPeg, char toPeg, int disk)
-    {
-        System.out.println("Move the disk " + disk + 
-                                " from " + fromPeg + 
-                                " to " + toPeg);
+    else if (p1 > p2){
+        push(source, p1);
+        push(source, p2);
+        System.out.println("Move the disk "+p2+" from "+d+" to "+s);    }
+    else{
+        push(destination, p2);
+        push(destination, p1);
+    System.out.println("Move the disk "+p1+ " from " +s+ " to "+d);
     }
-    
-    // Function to implement TOH puzzle
-    void tohIterative(int num_of_disks, Stack
-                    src, Stack aux, Stack dest)
-    {
-        int i, total_num_of_moves;
+    }
+    public static void main(String[] args){
+        Scanner us=new Scanner(System.in);
+        int num_of_disks = us.nextInt();
+        Main ob = new Main();
+        Stack source, destination, auxillary;
+        source = ob.createStack(num_of_disks);
+        destination = ob.createStack(num_of_disks);
+        auxillary = ob.createStack(num_of_disks);
+        int total_num_of_moves;
         char s = 'S', d = 'D', a = 'A';
-    
-        // If number of disks is even, then 
-        // interchange destination pole and
-        // auxiliary pole
-        if (num_of_disks % 2 == 0)
-        {
+        if (num_of_disks % 2 == 0){
             char temp = d;
             d = a;
-            a = temp;
-        }
-        total_num_of_moves = (int)(Math.pow(
-                            2, num_of_disks) - 1);
-    
-        // Larger disks will be pushed first
-        for(i = num_of_disks; i >= 1; i--)
-            push(src, i);
-    
-        for(i = 1; i <= total_num_of_moves; i++)
-        {
+            a = temp;}
+        total_num_of_moves = (int)(Math.pow(2, num_of_disks) - 1);
+        for(int i = num_of_disks; i >= 1; i--)
+            ob.push(source, i);
+        for(int i = 1; i <= total_num_of_moves; i++){
             if (i % 3 == 1)
-            moveDisksBetweenTwoPoles(src, dest, s, d);
-    
+                ob.move_disc(source, destination, s, d);
             else if (i % 3 == 2)
-            moveDisksBetweenTwoPoles(src, aux, s, a);
-    
+                ob.move_disc(source, auxillary, s, a);
             else if (i % 3 == 0)
-            moveDisksBetweenTwoPoles(aux, dest, a, d);
+                ob.move_disc(auxillary, destination, a, d);
         }
-    }
-    
-    // Driver code
-    public static void main(String[] args)
-    {
-        
-        // Input: number of disks
-        int num_of_disks = 3;
-        
-        IterativeTowerofHanai ob = new IterativeTowerofHanai();
-        Stack src, dest, aux;
-        
-        // Create three stacks of size 'num_of_disks'
-        // to hold the disks
-        src = ob.createStack(num_of_disks);
-        dest = ob.createStack(num_of_disks);
-        aux = ob.createStack(num_of_disks);
-        
-        ob.tohIterative(num_of_disks, src, aux, dest);
-    }
-    }
-    
-    // This code is contributed by Sumit Ghosh
-    
+        }
+        }        
